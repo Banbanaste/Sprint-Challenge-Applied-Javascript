793bc3b2abb50767ee099eb7d75f7a9ffb864071
Roberto Banbanaste <rbanbanaste@outlook.com>
2019-12-06 12:03:38 -0500

    carousel complete

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,72 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function Carousel() {
+  let imgNum = 1;
+
+  const carousel = document.createElement("div"),
+    left = document.createElement("div"),
+    img1 = document.createElement("img"),
+    img2 = document.createElement("img"),
+    img3 = document.createElement("img"),
+    img4 = document.createElement("img"),
+    right = document.createElement("div");
+
+  carousel.classList.add("carousel");
+  left.classList.add("left-button");
+  right.classList.add("right-button");
+  img1.classList.add("currentImg");
+  img1.classList.add("carousel-img");
+  img2.classList.add("carousel-img");
+  img3.classList.add("carousel-img");
+  img4.classList.add("carousel-img");
+
+  img1.id = "img1";
+  img2.id = "img2";
+  img3.id = "img3";
+  img4.id = "img4";
+
+  img1.src = "./assets/carousel/mountains.jpeg";
+  img2.src = "./assets/carousel/computer.jpeg";
+  img3.src = "./assets/carousel/trees.jpeg";
+  img4.src = "./assets/carousel/turntable.jpeg";
+
+  function setImg(num) {
+    const list = document.querySelectorAll(".carousel-img");
+    list.forEach(img => {
+      img.classList.remove("currentImg");
+    });
+
+    num === 1 && img1.classList.add("currentImg");
+    num === 2 && img2.classList.add("currentImg");
+    num === 3 && img3.classList.add("currentImg");
+    num === 4 && img4.classList.add("currentImg");
+  }
+
+  right.addEventListener("click", event => {
+    if (imgNum < 4) {
+      imgNum++;
+    } else {
+      imgNum = 1;
+    }
+    console.log("imgNum:", imgNum);
+    setImg(imgNum);
+  });
+
+  left.addEventListener("click", event => {
+    if (imgNum > 1) {
+      imgNum--;
+    } else {
+      imgNum = 4;
+    }
+    console.log("imgNum:", imgNum);
+    setImg(imgNum);
+  });
+
+  carousel.append(left, img1, img2, img3, img4, right);
+
+  return carousel;
+}
+
+document.querySelector(".carousel-container").appendChild(Carousel());
